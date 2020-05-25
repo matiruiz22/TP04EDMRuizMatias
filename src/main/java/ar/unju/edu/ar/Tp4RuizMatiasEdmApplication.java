48fa5ba8ca81b1112e7c18ade4aa4498ea30a51e
matul <matul@DESKTOP-7NH2C89>
2020-05-25 00:05:40 -0300

    inicializacion

---
+++ src/main/java/ar/unju/edu/ar/Tp4RuizMatiasEdmApplication.java
@@ -0,0 +1,13 @@
+package ar.unju.edu.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4RuizMatiasEdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4RuizMatiasEdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/unju/edu/ar/Tp4RuizMatiasEdmApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.unju.edu.ar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4RuizMatiasEdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
